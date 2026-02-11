e51cb78b6f22a17216c3b02d4b72850a294790d3
KiranAyan <royal.kiran318@gmail.com>
2026-02-11 11:49:41 +0530

    Confilct test 1

--- src/test/java/xpathSelenium/RunTestTrip.java
+++ src/test/java/xpathSelenium/RunTestTrip.java
@@ -3,6 +3,7 @@ package xpathSelenium;
 public class RunTestTrip {
 	public static void main(String[] args) {
 		TripDotCom TDC = new TripDotCom();
+		System.out.println("Running Trip.com Test...");
 		
 	}
 
